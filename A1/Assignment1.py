7438c8bfef237e23c72f382d768c56234ae89067
goldscace <118892544+goldscace@users.noreply.github.com>
2023-06-27 11:12:44 +0800

    Update Assignment1.py

--- A1/Assignment1.py
+++ A1/Assignment1.py
@@ -1,12 +1,11 @@
- 
-print("Enter 0 to 5 for the following options: ")
-print("0 -> Issue new ticket number")
-print("1 -> Assign ticket in queue for bill payment to Counter 1")
-print("2 -> Assign ticket in queue for bill payment to Counter 2")
-print("3 -> Assign ticket in queue for bill payment to Counter 3")
-print("4 -> Assign ticket in queue for bill payment to Counter 4")
-print("5 -> Quit Program")
-print(" ")
+def WhatToDo():
+    print("\nEnter 0 to 5 for the following options: ")
+    print("0 -> Issue new ticket number")
+    print("1 -> Assign ticket in queue for bill payment to Counter 1")
+    print("2 -> Assign ticket in queue for bill payment to Counter 2")
+    print("3 -> Assign ticket in queue for bill payment to Counter 3")
+    print("4 -> Assign ticket in queue for bill payment to Counter 4")
+    print("5 -> Quit Program")
 x=1000
 y=2000
 Bill=[]
@@ -16,131 +15,63 @@ c2=["No Ticket"]
 c3=["No Ticket"]
 c4=["No Ticket"]
 
-print("Ticket in queue for bill payment: ",Bill)
-print("Ticket in queue for license renew: ",License)
-print("Tickets at counter: Counter 1:",c1," Counter 2:",c2," Counter 3:",c3," Counter 4:",c4,)
-print("")
-
+def CurrentCounter():
+    print("\nTicket in queue for bill payment: ",Bill)
+    print("Ticket in queue for license renew: ",License)
+    print("Tickets at counter: Counter 1:",c1," Counter 2:",c2," Counter 3:",c3," Counter 4:",c4,"\n")
+    
+WhatToDo()
 while True:
-    option=(input("Enter your option: "))
+    CurrentCounter()
+    option=input("Enter your option: ")
     try:
-        option = int(option)
-        if option == 0:
-            print("")
-            print("Enter 0 or 1 for the following options:")
-            print("0 -> Bill payment")
-            print("1 -> License renew")
-            print("")
-            ticket=(input("Enter your option: "))
-            try:
-                ticket =int(ticket)
-                if ticket==0:
-                    x=x+1
-                    Bill.append(x)
-                    print("")
-                    print("Ticket in queue for Bill Payment: ",Bill)
-                    print("Ticket in queue for License Renew: ",License)
-                    print("Tickets at counter: Counter 1:",c1," Counter 2:",c2," Counter 3:",c3," Counter 4:",c4,)
-                    print("")
-                elif ticket==1:
-                    y=y+1
-                    License.append(y)
-                    print("")
-                    print("Ticket in queue for Bill Payment: ",Bill)
-                    print("Ticket in queue for License Renew: ",License)
-                    print("Tickets at counter: Counter 1:",c1," Counter 2:",c2," Counter 3:",c3," Counter 4:",c4,)
-                    print("")
-                else:
-                    print("")
-                    print("Invalid Number, pls try again")
-                    print("Ticket in queue for Bill Payment: ",Bill)
-                    print("Ticket in queue for License Renew: ",License)
-                    print("Tickets at counter: Counter 1:",c1," Counter 2:",c2," Counter 3:",c3," Counter 4:",c4,)
-                    print("")
-            except:
-                print("")
-                print("Invalid Number, pls try again")
-                print("Ticket in queue for Bill Payment: ",Bill)
-                print("Ticket in queue for License Renew: ",License)
-                print("Tickets at counter: Counter 1:",c1," Counter 2:",c2," Counter 3:",c3," Counter 4:",c4,)
-                print("")
-        elif option == 1:
-            if Bill==[]:
-                print("")
-                print("There is no ticket in queue")
-                print("Ticket in queue for Bill Payment: ",Bill)
-                print("Ticket in queue for License Renew: ",License)
-                print("Tickets at counter: Counter 1:",c1," Counter 2:",c2," Counter 3:",c3," Counter 4:",c4,)
-                print("")
-            else:
-                c1=Bill.pop(0)
-                print("")
-                print("Ticket in queue for Bill Payment: ",Bill)
-                print("Ticket in queue for License Renew: ",License)
-                print("Tickets at counter: Counter 1:",c1," Counter 2:",c2," Counter 3:",c3," Counter 4:",c4,)
-                print("")
-        elif option == 2:
-            if Bill==[]:
-                print("")
-                print("There is no ticket in queue")
-                print("Ticket in queue for Bill Payment: ",Bill)
-                print("Ticket in queue for License Renew: ",License)
-                print("Tickets at counter: Counter 1:",c1," Counter 2:",c2," Counter 3:",c3," Counter 4:",c4,)
-                print("")
-            else:
-                c2=Bill.pop(0)
-                print("")
-                print("Ticket in queue for Bill Payment: ",Bill)
-                print("Ticket in queue for License Renew: ",License)
-                print("Tickets at counter: Counter 1:",c1," Counter 2:",c2," Counter 3:",c3," Counter 4:",c4,)
-                print("")
-        elif option == 3:
-            if License==[]:
-                print("")
-                print("There is no ticket in queue")
-                print("Ticket in queue for Bill Payment: ",Bill)
-                print("Ticket in queue for License Renew: ",License)
-                print("Tickets at counter: Counter 1:",c1," Counter 2:",c2," Counter 3:",c3," Counter 4:",c4,)
-                print("")
-            else:
-                c3=License.pop(0)
-                print("")
-                print("Ticket in queue for Bill Payment: ",Bill)
-                print("Ticket in queue for License Renew: ",License)
-                print("Tickets at counter: Counter 1:",c1," Counter 2:",c2," Counter 3:",c3," Counter 4:",c4,)
-                print("")
-        elif option == 4:
-            if License==[]:
-                print("")
-                print("There is no ticket in queue")
-                print("Ticket in queue for Bill Payment: ",Bill)
-                print("Ticket in queue for License Renew: ",License)
-                print("Tickets at counter: Counter 1:",c1," Counter 2:",c2," Counter 3:",c3," Counter 4:",c4,)
-                print("")
-            else:
-                c4=License.pop(0)
-                print("")
-                print("Ticket in queue for Bill Payment: ",Bill)
-                print("Ticket in queue for License Renew: ",License)
-                print("Tickets at counter: Counter 1:",c1," Counter 2:",c2," Counter 3:",c3," Counter 4:",c4,)
-                print("")
-        elif option == 5:
-            print("")  
-            print("Quiting Program...")
-            break
-        else:
-            print("")
-            print("Invalid Number, pls try again")
-            print("Ticket in queue for Bill Payment: ",Bill)
-            print("Ticket in queue for License Renew: ",License)
-            print("Tickets at counter: Counter 1:",c1," Counter 2:",c2," Counter 3:",c3," Counter 4:",c4,)
-            print("")
+        option=int(option)
     except:
-        print("")
-        print("Invalid Number, pls try again")
-        print("Ticket in queue for Bill Payment: ",Bill)
-        print("Ticket in queue for License Renew: ",License)
-        print("Tickets at counter: Counter 1:",c1," Counter 2:",c2," Counter 3:",c3," Counter 4:",c4,)
-        print("")
+        print("\nInvalid Number, pls try again")
+        continue
+    if option == 0:
+        print("\nEnter 0 or 1 for the following options:")
+        print("0 -> Bill payment")
+        print("1 -> License renew\n")
+        ticket=input("Enter your option: ")
+        try:
+            ticket=int(ticket)
+        except:
+            print("\nInvalid Number, pls try again")
+            continue
+        if ticket==0:
+            x=x+1
+            Bill.append(x)
+        elif ticket==1:
+            y=y+1
+            License.append(y)
+        else:
+            print("\nInvalid Number, pls try again")
+    elif option == 1:
+        if Bill==[]:
+            print("\nThere is no ticket in queue")
+        else:
+            c1=Bill.pop(0)
+    elif option == 2:
+        if Bill==[]:
+            print("\nThere is no ticket in queue")
+        else:
+            c2=Bill.pop(0)
+    elif option == 3:
+        if License==[]:
+            print("\nThere is no ticket in queue")
+        else:
+            c3=License.pop(0)
+    elif option == 4:
+        if License==[]:
+            print("\nThere is no ticket in queue")
+        else:
+            c4=License.pop(0)
+    elif option == 5: 
+        print("\nQuiting Program...")
+        break
+    else:
+        print("\nInvalid Number, pls try again")
+
 
     
